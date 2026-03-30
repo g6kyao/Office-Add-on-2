e7e9f556423bf4f8ac82a4dad2b5b3fed6bcaafc
jyao <wei321.yao@gmail.com>
2026-03-30 18:27:48 +0200

    add color

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,11 +9,12 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("run-pink").onclick = run_pink;
+    document.getElementById("run-yellow").onclick = run_yellow;
   }
 });
 
-export async function run() {
+export async function run_pink() {
   try {
     await Excel.run(async (context) => {
       /**
@@ -26,6 +27,28 @@ export async function run() {
 
       // Update the fill color
       range.format.fill.color = "pink";
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function run_yellow() {
+  try {
+    await Excel.run(async (context) => {
+      /**
+       * Insert your Excel code here
+       */
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address
+      range.load("address");
+
+      // Update the fill color
+      range.format.fill.color = "yellow";
 
       await context.sync();
       console.log(`The range address was ${range.address}.`);
